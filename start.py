589f103e3ebad429f69e5d94c009484800671efd
Josh Malek <17jmalek98@gmail.com>
2021-04-18 13:42:07 -0400

    add interactive button and window icon

--- start.py
+++ start.py
@@ -1,13 +1,36 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
+from PyQt5 import QtGui
 import sys
+
+class EvacGui(QMainWindow):
+    def __init__(self):
+        super(EvacGui,self).__init__()
+        self.setWindowIcon(QtGui.QIcon('icon-circle.png'))
+        self.setGeometry(200,200,300,300)
+        self.setWindowTitle("EvacSim")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("placeholder")
+        self.label.move(50,50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("button pressed")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200,200,300,300)
-    win.setWindowTitle("EvacSim")
-
+    win = EvacGui()
     win.show()
     sys.exit(app.exec())
 window()

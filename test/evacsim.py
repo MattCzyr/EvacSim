7de9155266563f82e6fd9aee069f093f6092bb72
Matt Czyr <mattczyr@gmail.com>
2021-04-29 17:47:25 -0400

    Fix imports in simulation unit tests

--- test/evacsim.py
+++ test/evacsim.py
@@ -1,11 +1,11 @@
 import unittest
-import evacsim
+import evacsim.evacsim
 
 class TestEvacSim(unittest.TestCase):
 
     def test_load_models(self):
         """Tests the get_affected_nodes function"""
-        ev = evacsim.EvacSim()
+        ev = evacsim.evacsim.EvacSim()
         ev.load_models()
         self.assertGreater(len(ev.nodes), 0)
         self.assertGreater(len(ev.edges), 0)
@@ -13,7 +13,7 @@ class TestEvacSim(unittest.TestCase):
 
     def test_get_affected_nodes(self):
         """Tests the get_affected_nodes function"""
-        ev = evacsim.EvacSim()
+        ev = evacsim.evacsim.EvacSim()
         ev.load_models()
         for node in ev.nodes:
             for data in ev.disaster.data:
@@ -22,7 +22,7 @@ class TestEvacSim(unittest.TestCase):
     
     def test_get_connected_edges(self):
         """Tests the get_connected_edges function"""
-        ev = evacsim.EvacSim()
+        ev = evacsim.evacsim.EvacSim()
         ev.load_models()
         for edge in ev.edges:
             self.assertTrue(edge.source in ev.get_connected_edges(edge.dest))
@@ -30,7 +30,7 @@ class TestEvacSim(unittest.TestCase):
     
     def test_generate_evacuation_routes(self):
         """Tests the generate_evacuation_routes function"""
-        ev = evacsim.EvacSim()
+        ev = evacsim.evacsim.EvacSim()
         ev.load_models()
         ev.generate_evacuation_routes()
         for node in ev.get_affected_nodes():
